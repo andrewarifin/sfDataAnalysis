babecee4fb75ea3cbce0dbfd72769aa0488bbba5
Andrew Arifin <andrewarifin@Andrews-MacBook-Pro-2.local>
2017-10-03 02:55:42 -0700

    style update and render fix

--- app.py
+++ app.py
@@ -13,120 +13,224 @@ import statsmodels.api as sm
 
 app = dash.Dash()
 
+server = app.server
+
 app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
 
 app.config['suppress_callback_exceptions']=True
 
 df = pd.read_csv('./sfEnergyBenchmarks.csv', thousands=',')
 
+del df['SFPUC Code']
+del df['Notes']
+del df['Dpt.']
+del df['ENERGY STAR Bldg Type']
+del df['2013 ENERGY STAR rating ']
+del df['2013 Building Area (sq. ft.)']
+
+
 eui = '2013 Site EUI (kBtu/sq.ft.)'
 buildingArea = '2013 Building Area (sq. ft.)'
 yearBuilt = 'Year Built'
 buildingCategory = 'General Building Category'
 
-meanBuildingCategory_df = df.groupby([buildingCategory]).mean().reset_index().sort_values(eui)
+yearLabels = [
+    '1880-1889',
+    '1890-1899',
+    '1900-1909',
+    '1910-1919',
+    '1920-1929',
+    '1930-1939',
+    '1940-1949',
+    '1950-1959',
+    '1960-1969',
+    '1970-1979',
+    '1980-1989',
+    '1990-1999',
+    '2000-2009',]
 
-hospital_df = df[df[buildingCategory].isin(['Hospitals'])]
-hospital_labels = hospital_df['Facility']
+colorPalette = [
+    '#F09F8A',
+    '#EF9EA0',
+    '#E2A2B5',
+    '#CCA9C5',
+    '#AFB2CC',
+    '#92B9CA',
+    '#7DBFBC',
+    '#78C2A7',
+    '#85C18E',
+    '#9CBE77',
+    '#B7B866',
+    '#D2AF61',
+    '#E9A569',
+    '#F89C7B',
+]
 
-@app.callback (
-    dash.dependencies.Output('pie-chart-with-dropdown', 'figure'),
-    [dash.dependencies.Input('dropdown', 'value')]
-)
-def updatePieChart(selectedCategory):
-    if (selectedCategory == 'General'):
-        trace = [
-            go.Bar(
-                x=meanBuildingCategory_df[buildingCategory],
-                y=meanBuildingCategory_df[eui],
-                name='EUI by Building Category',
+def generateGeneralGraph():
+    meanBuildingCategory_df = df.groupby([buildingCategory]).mean().reset_index().sort_values(eui)
+
+    trace = [
+        go.Bar(
+            x=meanBuildingCategory_df[buildingCategory],
+            y=meanBuildingCategory_df[eui],
+            marker=dict(
+                color=colorPalette
             )
-        ]
-        return {
-            'data': trace,
-            'id': 'bar-graph',
+        )
+    ]
+    return html.Div([
+        dcc.Graph(
+            figure={
+                'data': trace,
+                'layout': go.Layout(
+                    title='EUI by Building Category',
+                    xaxis=dict(
+                        title='Building Category'
+                    ),
+                    yaxis=dict(
+                        title=eui
+                    ),
+                )
+            },
+            id='generalBarGraph'
+        )
+    ])
 
-        }
-    category_df = df[df[buildingCategory].isin([selectedCategory])].head(10)
+def generatePieChart(category):
+    category_df = df[df[buildingCategory].isin([category])].head(10)
     labels = category_df['Facility']
 
     trace = [
-        go.Pie(labels=labels, values=category_df[eui]),
+        go.Pie(
+            labels=labels,
+            values=category_df[eui],
+        ),
     ]
 
-    return {
-        'data': trace,
-        'id': 'pie-chart',
-        'layout': go.Layout(
-            title=selectedCategory,
-            showlegend=True,
-            legend=go.Legend(
-                x=1,
-                y=1,
-                font=dict(
-                    size=8
-                )
-            ),
-            margin=go.Margin(l=0, r=0, t=60, b=0),
-        )
-    }
+    return html.Div([
+        dcc.Graph(figure={
+            'data': trace,
+            'layout': go.Layout(
+                title='Top 10 EUI for %s' % category,
+                showlegend=True,
+                legend=go.Legend(
+                    x=1,
+                    y=1,
+                    font=dict(
+                        size=8
+                    )
+                ),
+                margin=go.Margin(l=0, r=0, t=60, b=0),
+            )
+        }, id='pieChart')
+    ], className= '' if category == 'Hospitals' else 'six columns')
 
-@app.callback (
-    dash.dependencies.Output('table-div', 'children'),
-    [dash.dependencies.Input('dropdown', 'value')]
-)
-def updateTable(selectedCategory):
-    if (selectedCategory == 'General'):
-        return None
-    category_df = df[df[buildingCategory].isin([selectedCategory])].head(10)
-    return html.Table(
-        [html.Tr([html.Th(col) for col in category_df.columns])] +
-
-        [html.Tr([
-            html.Td(category_df.iloc[i][col]) for col in category_df.columns
-        ]) for i in range(0,len(category_df))]
-    )
-
-@app.callback (
-    dash.dependencies.Output('bar-graph-with-dropdown', 'figure'),
-    [dash.dependencies.Input('dropdown', 'value')]
-)
-def updateBarGraph(selectedCategory):
-    if (selectedCategory == 'General' or selectedCategory == 'Hospitals'):
-        return {
-            'data': [],
-            'id': 'facility-chart'
-        }
-    category_df = df[df[buildingCategory].isin([selectedCategory])]
+def generateCategoryBarGraph(category):
+    category_df = df[df[buildingCategory].isin([category])]
+    category_df['Facility Type'] = category_df['Facility Type'].str.lower()
     byFacilityType_df = category_df.groupby('Facility Type').mean()
+    byCategory = byFacilityType_df.unstack()[eui]
     trace = [
         go.Bar (
-            x=category_df['Facility Type'],
-            y=category_df[eui]
+            x=byCategory.index,
+            y=byCategory,
+            marker=dict(
+                color=colorPalette
+            )
         )
     ]
 
-    return {
-        'data': trace,
-        'id': 'facility-chart'
-    }
+    return html.Div([
+        dcc.Graph(figure={
+            'data': trace,
+            'layout': go.Layout(
+                title='Average EUI by Facility Type',
+                xaxis=dict(
+                    title='Facility Type'
+                ),
+                yaxis=dict(
+                    title='Average %s' % eui
+                ),
+            )
+        }, id='categoryBarGraph')
+    ], className='six columns')
+
+def generateTable(category):
+    if (category == 'General'):
+        return None
+    category_df = df[df[buildingCategory].isin([category])].head(10)
+    return html.Div([
+        html.Table(
+            [html.Tr([html.Th(col) for col in category_df.columns])] +
+
+            [html.Tr([
+                html.Td(category_df.iloc[i][col]) for col in category_df.columns
+            ]) for i in range(0,len(category_df))]
+        )
+    ], className='six columns')
+
+def generateYearRangeGraph(category):
+    if (category == 'General'):
+        return None
+    category_df = df[df[buildingCategory].isin([category])]
+    category_df['Year Renov.'].fillna(category_df[yearBuilt], inplace=True)
+    category_df.rename(columns={'Year Renov.': 'Year Built/Renovated'}, inplace=True)
+    category_df['year_range'] = pd.cut(category_df['Year Built/Renovated'], range(1880, 2011, 10), right=False, labels=yearLabels)
+    byYear_df = category_df.groupby(['year_range']).mean()
+    byYear = byYear_df.unstack()[eui]
+
+    div = html.Div([
+        dcc.Graph(
+            figure = {
+                'data': [
+                    go.Bar(
+                        x=byYear.index,
+                        y=byYear,
+                        marker=dict(
+                            color=colorPalette
+                        )
+                    )
+                ],
+                'layout': go.Layout(
+                    title='Average EUI by Construction/Renovation Year',
+                    xaxis=dict(
+                        title='Construction/Renovation Year'
+                    ),
+                    yaxis=dict(
+                        title='Average %s' % eui
+                    )
+                )
+            }, id='lineChart'
+        )
+    ], className='six columns')
+
+    return div
 
 @app.callback(
-    dash.dependencies.Output('pie-chart-column', 'className'),
+    dash.dependencies.Output('pieBarRow', 'children'),
     [dash.dependencies.Input('dropdown', 'value')]
 )
-def updateBarGraphFormating(selectedCategory):
-    if (selectedCategory == 'General' or selectedCategory == 'Hospitals'):
-        return ''
-    return 'six columns'
+def generateGraphs(selectedCategory):
+    if (selectedCategory == 'General'):
+        return [generateGeneralGraph()]
+    if (selectedCategory == 'Hospitals'):
+        return generatePieChart(selectedCategory)
+    return [
+        generatePieChart(selectedCategory),
+        generateCategoryBarGraph(selectedCategory),
+    ]
 
 @app.callback(
-    dash.dependencies.Output('bar-graph-with-dropdown', 'style'),
+    dash.dependencies.Output('yearTableRow', 'children'),
     [dash.dependencies.Input('dropdown', 'value')]
 )
-def updateBarGraphDisplay(selectedCategory):
-    if (selectedCategory == 'General' or selectedCategory == 'Hospitals'):
-        return {'display': 'none'}
+def generateTableAndChart(selectedCategory):
+    if (selectedCategory == 'General'):
+        return None
+    return [
+        generateYearRangeGraph(selectedCategory),
+        generateTable(selectedCategory),
+    ]
 
 app.layout = html.Div([
     dcc.Dropdown(
@@ -141,40 +245,8 @@ app.layout = html.Div([
         ],
         value='General'
     ),
-    html.Div([
-        html.Div([
-            dcc.Graph(id='pie-chart-with-dropdown'),
-        ], id='pie-chart-column'),
-        html.Div([
-            dcc.Graph(id='bar-graph-with-dropdown'),
-        ], className='six columns'),
-    ]),
-    html.Div(id='table-div')
-    # dcc.Graph(
-    #     figure=go.Figure(
-    #         data=[
-    #             go.Bar(
-    #                 x=meanBuildingCategory_df[buildingCategory],
-    #                 y=meanBuildingCategory_df[eui],
-    #                 name='EUI by Building Category',
-    #                 marker=go.Marker(
-    #                     color='rgb(55, 83, 109)'
-    #                 )
-    #             )
-    #         ],
-            # layout=go.Layout(
-            #     title='EUI by Building Category',
-            #     showlegend=True,
-            #     legend=go.Legend(
-            #         x=0,
-            #         y=1.0
-            #     ),
-            #     margin=go.Margin(l=40, r=0, t=40, b=30)
-            # )
-    #     ),
-    #
-    #     id='my-graph'
-    # ),
+    html.Div(id='pieBarRow', className='row'),
+    html.Div(id='yearTableRow', className='row'),
 ])
 
 if __name__ == '__main__':
